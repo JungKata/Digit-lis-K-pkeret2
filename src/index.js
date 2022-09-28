8cd5ce33cc82167c69a7c8df1cbde772018dddc4
JungKata <97729333+JungKata@users.noreply.github.com>
2022-09-28 13:46:18 +0200

    java eredmeny kiosztas

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import './style.css';
 
+let kep_url = document.getElementById("kep").value;
+let pixel = parseFloat(document.getElementById("szelesseg").value);
+let magas = parseFloat(document.getElementById("magassag").value);
+let szin = document.getElementById("szinezes").value;
+
+
+function beallit()
+{
+    document.getSelection
+}
