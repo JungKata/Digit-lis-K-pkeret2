0e8126453e5cef13e69018dbd4d8aa5cc40a8b97
JungKata <97729333+JungKata@users.noreply.github.com>
2022-09-29 19:43:17 +0200

    kész

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './style.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 /*let pixel = parseFloat(document.getElementById("szelesseg").value);
@@ -6,18 +8,24 @@ let magas = parseFloat(document.getElementById("magassag").value);
 let szin = document.getElementById("szinezes").value;
 */
 
-function beallit_url()
+function beallit_url(e)
 {
-    document.getElementById("kep_elhelyez").src = document.getElementById("kep");
+    document.getElementById('kep_elhelyez').src = e.currentTarget.value;
 
 }
-function beallit_szelesseg(){
-    document.getElementById("kep_elhelyez").style.width = parseInt(document.getElementById("szelesseg").value) +"px";
+function beallit_szelesseg(e){
+    document.getElementById('kep_elhelyez').width = e.currentTarget.value;
 }
 
-function beallit_magassag()
+function beallit_keret(e)
 {
-    document.getElementById("kep_elhelyez").style.height = parent(document.getElementById("magassag").value)
+    document.getElementById("kep_elhelyez").style.border = parent(document.getElementById("keret").value)
 }
+
+function init()
+{
 document.getElementById('kep').addEventListener('change', beallit_url);
 document.getElementById('kep').addEventListener('input', beallit_szelesseg);
+}
+
+document.addEventListener('DOMContentLoaded',init)
