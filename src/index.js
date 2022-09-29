ebdc13305cd92224fec85c84604faba8d2b7f0c0
JungKata <97729333+JungKata@users.noreply.github.com>
2022-09-29 19:42:41 +0200

    kész

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import './style.css';
 
-let kep_url = document.getElementById("kep").value;
-let pixel = parseFloat(document.getElementById("szelesseg").value);
+
+/*let pixel = parseFloat(document.getElementById("szelesseg").value);
 let magas = parseFloat(document.getElementById("magassag").value);
 let szin = document.getElementById("szinezes").value;
+*/
 
-
-function beallit()
+function beallit_url()
 {
-    document.getElementById("kep_elhelyez").src = kep_url;
+    document.getElementById("kep_elhelyez").src = document.getElementById("kep");
+
 }
+function beallit_szelesseg(){
+    document.getElementById("kep_elhelyez").style.width = parseInt(document.getElementById("szelesseg").value) +"px";
+}
+
+function beallit_magassag()
+{
+    document.getElementById("kep_elhelyez").style.height = parent(document.getElementById("magassag").value)
+}
+document.getElementById('kep').addEventListener('change', beallit_url);
+document.getElementById('kep').addEventListener('input', beallit_szelesseg);
